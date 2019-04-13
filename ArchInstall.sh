8d66372e73fc20612dbdfb9184077743a9dff383
charl-potgieter <charl.j.potgieter@gmail.com>
2019-04-13 22:32:32 +1000

    Update ArchInstall.sh

--- ArchInstall.sh
+++ ArchInstall.sh
@@ -15,7 +15,6 @@ read -p "Enter host name : " HOSTNAME
 # read -p "Enter IP address : " IPADDRESS
 read -p "Enter network interface e.g. enp0s3, eth0 : " INTERFACE
 
-
 	
 # Disk partitioning and formatting
 parted /dev/sda mklabel msdos
@@ -23,12 +22,17 @@ parted /dev/sda mkpart primary ext4 1MiB 100%
 parted /dev/sda set 1 boot on
 mkfs.ext4 /dev/sdX1
 
+# Mount the partition
+mount /dev/sda1 /mnt
 
-# GRUB Bootloader
-pacman -S grub
-grub-install --target=i386-pc /dev/sda
-grub-mkconfig -o /boot/grub/grub.cfg
+# Install base package
+pacstrap /mnt base
 
+# Generate fstab
+genfstab -U /mnt >> /mnt/etc/fstab
+
+# chroot into new system
+arch-chroot /mnt
 
 
 # Update clock and set time zones
@@ -53,3 +57,10 @@ echo  127.0.1.1 "    " $HOSTNAME".localdomain   " $HOSTNAME >> /etc/hosts
 
 
 ip link set $INTERFACE up
+
+
+# GRUB Bootloader
+pacman -S grub
+grub-install --target=i386-pc /dev/sda
+grub-mkconfig -o /boot/grub/grub.cfg
+
