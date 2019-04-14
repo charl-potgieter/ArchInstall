4ae8be58f7d84edb6000ca2bdeebc8f470cb6360
charl-potgieter <charl.j.potgieter@gmail.com>
2019-04-14 19:51:47 +1000

    Create Temp.sh

---
+++ Temp.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo '--------------------------------------------------------------------------------------------'
+echo '        Allow editing of sudo file'
+echo '--------------------------------------------------------------------------------------------'
+
+printf '\n'
+echo 'editing with visudo.   Uncomment  %wheel    ALL=(ALL) ALL'
+read -p "Press enter to continue... "
+visudo
