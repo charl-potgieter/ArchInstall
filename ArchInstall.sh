b32f9c3827d84db2466e91f1a2714c91c8cbae1f
charl-potgieter <charl.j.potgieter@gmail.com>
2019-04-14 20:24:43 +1000

    Update ArchInstall.sh

--- ArchInstall.sh
+++ ArchInstall.sh
@@ -126,6 +126,33 @@ systemctl enable smb.service
 systemctl enable nmb.service
 
 
+
+echo '--------------------------------------------------------------------------------------------'
+echo '        Select the  root password'
+echo '--------------------------------------------------------------------------------------------'
+printf '\n'
+passwd
+
+
+echo '--------------------------------------------------------------------------------------------'
+echo '        Allow editing of sudo file'
+echo '--------------------------------------------------------------------------------------------'
+
+printf '\n'
+echo 'editing with visudo.   Uncomment  %wheel    ALL=(ALL) ALL'
+read -p "Press enter to continue... "
+visudo
+
+
+echo '--------------------------------------------------------------------------------------------'
+echo '        Add user charl (with home directory) and set password and add to wheel group'
+echo '--------------------------------------------------------------------------------------------'
+
+useradd -m charl
+passwd charl
+gpasswd -a charl wheel
+
+
 exit # to leave the chroot
 EOF
 #######################################################################################################################
