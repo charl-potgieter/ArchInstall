a4fa7db0647e8ddf22e3bc4216b84b98c0838f63
charl-potgieter <charl.j.potgieter@gmail.com>
2019-04-16 21:45:11 +1000

    Create PostInstallationServer.sh

---
+++ PostInstallationServer.sh
@@ -0,0 +1,12 @@
+# ----------------------------------------
+#   To Do
+# ----------------------------------------
+
+# install guest additions-nox
+# create share folder on virtualbox gui called server referenced to below dropbox folder:
+#   C:\Users\charl\Dropbox\Dropbox_Charl\Computer_Technical_ExcludingCode\Linux_System_Files\Server
+
+# manual mount with 
+#    mount -t vboxsf shared_folder_name mount_point_on_guest_system
+
+# Copy the configuration files over to the virtual machine etc folder
