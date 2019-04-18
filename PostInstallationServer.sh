4e698a14e519d173f5688ee4b3c14c5e715afe0c
charl-potgieter <charl.j.potgieter@gmail.com>
2019-04-18 19:52:52 +1000

    Update PostInstallationServer.sh

--- PostInstallationServer.sh
+++ PostInstallationServer.sh
@@ -1,14 +1,11 @@
-# ----------------------------------------
-#   To Do
-# ----------------------------------------
 
 # RUN AS SUDO
 
-# install guest additions-nox
-# create share folder on virtualbox gui called server referenced to below dropbox folder:
-#   C:\Users\charl\Dropbox\Dropbox_Charl\Computer_Technical_ExcludingCode\Linux_System_Files\Server
+pacman -S guest-utils-nox
 
-# manual mount with 
-#    mount -t vboxsf shared_folder_name mount_point_on_guest_system
+# mount shared folder and copy files
+mkdir /mnt/temp
+mount -t vboxsf ConfigFiles /mnt/temp
 
 # Copy the configuration files over to the virtual machine etc folder
+cp /mnt/temp/smb.conf /etc/samba/smb.conf
