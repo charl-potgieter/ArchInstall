33fec3f17c7e759443422adcff8a702fc11ab333
Charl Potgieter <charl.j.potgieter@gmail.com>
2019-10-10 21:38:10 +1100

    start work on arch virt

--- PostInstallationServer.sh
+++ PostInstallationServer.sh
@@ -20,6 +20,8 @@ rm /pkg.list.temp
 printf '\n\n\n'
 
 
+
+
 echo '--------------------------------------------------------------------------------------------'
 echo '        Allow editing of sudo file'
 echo '--------------------------------------------------------------------------------------------'
@@ -31,78 +33,16 @@ EDITOR=vim visudo
 printf '\n\n\n'
 
 
-echo '--------------------------------------------------------------------------------------------'
-echo '			Copy configuration and user files from shared folder on local machine'
-echo '--------------------------------------------------------------------------------------------'
-
-# The copying of users inspired by the below
-# https://www.cyberciti.biz/faq/howto-move-migrate-user-accounts-old-to-new-server/
-
-# mount shared folder and copy files
-mkdir /mnt/temp
-mount -t vboxsf ConfigFiles /mnt/temp
-
-
-# Copy the configuration and user files over to the virtual machine etc folder
-cp /mnt/temp/smb.conf /etc/samba/smb.conf
-cp /mnt/temp/fstab_live_system /etc/fstab_live_system
-cat /mnt/temp/passwd.mig >> /etc/passwd
-cat /mnt/temp/group.mig >> /etc/group
-cat /mnt/temp/shadow.mig >> /etc/shadow
-cp /mnt/temp/gshadow.mig /etc/gshadow
-cp /mnt/temp/smbpasswd /usr/bin/smbpasswd
-
-
-
-printf '\n\n\n'
-
-
-echo '--------------------------------------------------------------------------------------------'
-echo '			Set file permissions of configuration file'
-echo '--------------------------------------------------------------------------------------------'
-
-
-# Set the file permissions
-chmod 644 /etc/samba/smb.conf
-chmod 644 /etc/group
-chmod 600 /etc/shadow
-chmod 600 /etc/gshadow
-chmod 755 /usr/bin/smbpasswd
-chmod 644 /etc/fstab_live_system
-
-
-printf '\n\n\n'
-
-
 
 echo '--------------------------------------------------------------------------------------------'
 echo '			Create directories for mounting data and backups in the live system'
 echo '--------------------------------------------------------------------------------------------'
 
 mkdir /srv/samba
+mkdir /mnt/backups
 mkdir /mnt/backups/usb_server_os
 mkdir /mnt/backups/mirror_overnight
 mkdir /mnt/backups/snapshots
-
-
-echo '--------------------------------------------------------------------------------------------'
-echo '			Copy public key from shared folder on local pc'
-echo '--------------------------------------------------------------------------------------------'
-
-# below user needs to have already have been copied across in config files
-read -p "Enter user for home folder creation for ssh (User needs to exist in config files) : " SSHUSER
-mkdir /home/$SSHUSER
-mkdir /home/$SSHUSER/.ssh
-cat /mnt/temp/id_rsa.pub >> /home/$SSHUSER/.ssh/authorized_keys
-
-chown $SSHUSER:$SSHUSER /home/$SSHUSER
-chown $SSHUSER:$SSHUSER /home/$SSHUSER/.ssh
-chown $SSHUSER:$SSHUSER /home/$SSHUSER/.ssh/authorized_keys
-chmod 700 /home/$SSHUSER/.ssh
-chmod 600 /home/$SSHUSER/.ssh/authorized_keys
-
-
-printf '\n\n\n'
 
 
 
@@ -117,13 +57,6 @@ vim /etc/ssh/sshd_config
 printf '\n\n\n'
 
 
-
-echo '--------------------------------------------------------------------------------------------'
-echo '			Unmount shared folder'
-echo '--------------------------------------------------------------------------------------------'
-
-umount /mnt/temp
-printf '\n\n\n'
 
 
 echo '--------------------------------------------------------------------------------------------'
---
+++ PostInstallationVirt.sh
@@ -0,0 +1,113 @@
+# RUN AS SUDO
+
+
+echo '--------------------------------------------------------------------------------------------'
+echo '			Systems upgrade'
+echo '--------------------------------------------------------------------------------------------'
+
+pacman -Syu
+printf '\n\n\n'
+
+
+echo '--------------------------------------------------------------------------------------------'
+echo '			Get and install server program list'
+echo '--------------------------------------------------------------------------------------------'
+
+curl -L https://raw.githubusercontent.com/charl-potgieter/ArchInstall/master/pkg.list.virt > /pkg.list.temp
+pacman -S --needed - < /pkg.list.temp
+rm /pkg.list.temp
+
+printf '\n\n\n'
+
+
+
+
+echo '--------------------------------------------------------------------------------------------'
+echo '        Allow editing of sudo file'
+echo '--------------------------------------------------------------------------------------------'
+
+printf '\n'
+echo 'editing with visudo.   Uncomment  %wheel    ALL=(ALL) ALL'
+read -p "Press enter to continue... "
+EDITOR=vim visudo
+printf '\n\n\n'
+
+
+echo '--------------------------------------------------------------------------------------------'
+echo '			Create new user and add to wheel'
+echo '--------------------------------------------------------------------------------------------'
+
+read -p "Enter user name (to be added to wheel group and home dir created) : " MYUSERNAME
+useradd -m $MYUSERNAME
+gpasswd -a $MYUSERNAME wheel
+printf '\n\n\n'
+
+
+echo '--------------------------------------------------------------------------------------------'
+echo '			Set user password'
+echo '--------------------------------------------------------------------------------------------'
+
+passwd $MYUSERNAME
+printf '\n\n\n'
+
+
+
+
+echo '--------------------------------------------------------------------------------------------'
+echo '			Make directories for mounting samba shares'
+echo '--------------------------------------------------------------------------------------------'
+
+mkdir /home/$MYUSERNAME/Documents_Charl
+mkdir /home/$MYUSERNAME/Documents_Kerrie
+mkdir /home/$MYUSERNAME/Documents_Dylan
+mkdir /home/$MYUSERNAME/Documents_Jared
+mkdir /home/$MYUSERNAME/Pictures
+mkdir /home/$MYUSERNAME/HomeVideos
+mkdir /home/$MYUSERNAME/Videos
+mkdir /home/$MYUSERNAME/Music
+mkdir /home/$MYUSERNAME/ZZZ_Backup_Cloud_Drives
+mkdir /home/$MYUSERNAME/ZZZ_Backup_USB_ServerOS
+mkdir /home/$MYUSERNAME/ZZZ_BackupMirrorOvernight
+mkdir /home/$MYUSERNAME/ZZZ_BackupSnapshots
+
+
+
+echo '--------------------------------------------------------------------------------------------'
+echo '			Change ownership of above directories'
+echo '--------------------------------------------------------------------------------------------'
+
+chown charl:charl /home/$MYUSERNAME/Documents_Charl
+chown charl:charl /home/$MYUSERNAME/Documents_Kerrie
+chown charl:charl /home/$MYUSERNAME/Documents_Dylan
+chown charl:charl /home/$MYUSERNAME/Documents_Jared
+chown charl:charl /home/$MYUSERNAME/Pictures
+chown charl:charl /home/$MYUSERNAME/HomeVideos
+chown charl:charl /home/$MYUSERNAME/Videos
+chown charl:charl /home/$MYUSERNAME/Music
+chown charl:charl /home/$MYUSERNAME/ZZZ_Backup_Cloud_Drives
+chown charl:charl /home/$MYUSERNAME/ZZZ_Backup_USB_ServerOS
+chown charl:charl /home/$MYUSERNAME/ZZZ_BackupMirrorOvernight
+chown charl:charl /home/$MYUSERNAME/ZZZ_BackupSnapshots
+
+
+
+
+echo '--------------------------------------------------------------------------------------------'
+echo '			Disable ssh passwords'
+echo '--------------------------------------------------------------------------------------------'
+
+echo 'set PasswordAuthentication no in sshd_config'
+read -p "Press enter to continue and make above change... "
+vim /etc/ssh/sshd_config
+
+printf '\n\n\n'
+
+
+
+
+echo '--------------------------------------------------------------------------------------------'
+echo '			Enable and start daemons'
+echo '--------------------------------------------------------------------------------------------'
+
+systemctl enable sshd.service
+systemctl start sshd.service
